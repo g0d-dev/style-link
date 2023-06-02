40cbe0199925c57e806c82b61aacd116395416be
kimyonghui <kyhui1115@gmail.com>
2023-06-02 20:47:16 +0900

    feat: MSW handler 기능 추가

--- src/components/header/DropdownListItem.jsx
+++ src/components/header/DropdownListItem.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import baseInstance from "../../api";
 
 DropdownListItem.propTypes = {
   result: PropTypes.string,
 };
 
 function DropdownListItem({ result }) {
-  return <div className="mt-2 ml-2 text-sm cursor-pointer">{result}</div>;
+  const handleClick = async () => {
+    const a = await baseInstance.get(`/main/search/${result}`);
+    console.log(a);
+  };
+  return (
+    <div className="mt-2 ml-2 text-sm cursor-pointer" onClick={handleClick}>
+      {result}
+    </div>
+  );
 }
 
 export default DropdownListItem;
